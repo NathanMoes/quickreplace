38a84aa61de7bb227e5c771663a6d61c016e0acd
Nathan Moes <nathanmoes7@gmail.com>
2023-06-24 10:54:49 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use text_colorizer::*;
 use std::env;
 use std::fs;
+use regex::Regex;
 
 #[derive(Debug)]
 struct Arguments{
@@ -25,7 +26,17 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data){
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace test: {:?}",
+                "Error:".red().bold(), e);
+            std::process::exit(1)
+        }
+    };
+
+
+    match fs::write(&args.output, &replaced_data){
         Ok(_) => {},
         Err(e) => {
             eprintln!("{} failed to write to file '{}': {:?}",
@@ -59,4 +70,9 @@ fn parse_args() -> Arguments{
         filename: args[2].clone(), 
         output: args[3].clone() 
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error>{
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
